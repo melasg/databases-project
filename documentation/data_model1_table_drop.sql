eb61827ab53215a6fa4a02d23dea0ba05e894c49
melasq <masgh003@ucr.edu>
2021-07-07 21:20:29 -0700

    finished starUML diagram, finished DB Schema, and generated base code

---
+++ documentation/data_model1_table_drop.sql
@@ -0,0 +1,6 @@
+DROP TABLE public.customer CASCADE;
+DROP TABLE public.servicerequest CASCADE;
+DROP TABLE public.mechanic CASCADE;
+DROP TABLE public.cars CASCADE;
+DROP TABLE public.invoice CASCADE;
+DROP TABLE public.owner CASCADE;
